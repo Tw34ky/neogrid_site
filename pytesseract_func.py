a5b8ab6fe8e429f25f7ca4b93ede6b9f830c8b2a
Timofey Kalyak <kalyakta@yandex.ru>
2025-07-11 23:59:55 +0700

    Update pytesseract_func.py
    added image preprocessing

--- pytesseract_func.py
+++ pytesseract_func.py
@@ -1,8 +1,7 @@
 import pytesseract
-from PIL import Image
+from PIL import Image, ImageFilter, ImageEnhance
 import os
 import fitz
-import pprint
 
 
 def pdf_to_text(pdf_path):
@@ -13,6 +12,12 @@ def pdf_to_text(pdf_path):
         for page in doc:
             pixmap = page.get_pixmap()
             image = Image.frombytes("RGB", (pixmap.width, pixmap.height), pixmap.samples)
+            if pixmap.width * pixmap.height > 2073600:
+                image.thumbnail((pixmap.width * 0.8, pixmap.height * 0.8))
+            image = image.filter(ImageFilter.MedianFilter())
+            image = image.convert("L")
+            enhancer = ImageEnhance.Contrast(image)
+            image = enhancer.enhance(2)
             temp = pytesseract.image_to_string(image, config=r'--oem 1 --psm 3', lang='eng+rus')
             text += temp
     return text
